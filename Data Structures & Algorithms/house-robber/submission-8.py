ba14a130561cd8474d697813bdf1c6de146ac4ae
Viktor Lado Naess <157009977+ViktorLadoNaess@users.noreply.github.com>
2026-06-24 06:33:37 -0700

    Add: house-robber - submission-8

---
+++ Data Structures & Algorithms/house-robber/submission-8.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n_1 = 0
+        n_2 = 0
+        for i in nums:
+            curr = max(n_1, n_2+i)
+            n_2 = n_1
+            n_1 = curr
+        return curr
